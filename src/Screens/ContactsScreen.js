9dce8419c13708130527f566fbc8bad6344c683e
msm <martinconor25@yahoo.com>
2022-08-24 01:03:21 +0300

    refactoring

--- src/Screens/ContactsScreen.js
+++ src/Screens/ContactsScreen.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native'
-
+import firestore from '@react-native-firebase/firestore';
+import { useRoute } from "@react-navigation/native";
+import React, { useContext, useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import ListItem from "../components/ListItem";
+import { useDispatch, useSelector } from 'react-redux';
+import { setRooms, setUnfilteredRooms } from '../Reducers/chatsSlice';
+import useContacts from "../hooks/useHooks";
+const users = firestore().collection('users');
 export const contactsIcon = () => {
     return (
         <View style={{ justifyContent: 'center', alignItems: 'center' }}>
@@ -18,9 +24,43 @@ export const ContactsScreenTobBar = () => {
     )
 }
 export const ContactsScreen = () => {
+    const contacts = useContacts();
+    const route = useRoute();
+    const image = route.params && route.params.image;
     return (
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Contacts Screen</Text>
-        </View>
-    )
+        <FlatList
+            style={{ flex: 1, padding: 10 }}
+            data={contacts}
+            keyExtractor={(_, i) => i}
+            renderItem={({ item }) => <ContactPreview contact={item} image={image} />}
+        />
+    );
+}
+
+function ContactPreview({ contact, image }) {
+
+    const [user, setUser] = useState(contact);
+    const { unfilteredRooms, rooms } = useSelector(state => state.chats);
+    useEffect(() => {
+        let query = users.where("email", "==", contact.email);
+        query.onSnapshot(snap => {
+
+            if (snap.docs.length) {
+                const userDoc = snap.docs[0].data();
+                setUser((prevUser) => ({ ...prevUser, userDoc }));
+            }
+        })
+
+    }, []);
+    return (
+        <ListItem
+            style={{ marginTop: 7 }}
+            type="contacts"
+            user={user}
+            image={image}
+            room={unfilteredRooms.find((room) =>
+                room.participantsArray.includes(contact.email)
+            )}
+        />
+    );
 }
--- src/utils.js
+++ src/utils.js
@@ -1,44 +1,50 @@
-import * as ImagePicker from "expo-image-picker";
+import * as ImagePicker from "react-native-image-picker";
 import "react-native-get-random-values";
 import { nanoid } from 'nanoid'
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
-import { storage } from "./firebase"
+import storage from '@react-native-firebase/storage';
 export async function pickImage() {
-    let result = ImagePicker.launchCameraAsync();
+    let result = await ImagePicker.launchCamera({
+        selectionLimit: 1,
+        mediaType: 'photo',
+        includeBase64: false,
+    });
     return result;
 }
-export async function askForPermission() {
-    const { status } = await ImagePicker.requestCameraPermissionsAsync();
-    return status;
-}
+// export async function askForPermission() {
+//     const { status } = await ImagePicker.requestCameraPermissionsAsync();
+//     return status;
+// }
 
 export async function uploadImage(uri, path, fName) {
+
     // Why are we using XMLHttpRequest? See:
     // https://github.com/expo/expo/issues/2402#issuecomment-443726662
-    const blob = await new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.onload = function () {
-            resolve(xhr.response);
-        };
-        xhr.onerror = function (e) {
-            console.log(e);
-            reject(new TypeError("Network request failed"));
-        };
-        xhr.responseType = "blob";
-        xhr.open("GET", uri, true);
-        xhr.send(null);
-    });
+    // const blob = await new Promise((resolve, reject) => {
+    //     const xhr = new XMLHttpRequest();
+    //     xhr.onload = function () {
+    //         resolve(xhr.response);
+    //     };
+    //     xhr.onerror = function (e) {
+    //         console.log(e);
+    //         reject(new TypeError("Network request failed"));
+    //     };
+    //     xhr.responseType = "blob";
+    //     xhr.open("GET", uri, true);
+    //     xhr.send(null);
+    // });
 
     const fileName = fName || nanoid();
-    const imageRef = ref(storage, `${path}/${fileName}.jpeg`);
+    // const imageRef = ref(storage, `${path}/${fileName}.jpeg`);
 
-    const snapshot = await uploadBytes(imageRef, blob, {
-        contentType: "image/jpeg",
-    });
+    // const snapshot = await uploadBytes(imageRef, blob, {
+    //     contentType: "image/jpeg",
+    // });
 
-    blob.close();
+    // blob.close();
 
-    const url = await getDownloadURL(snapshot.ref);
+    const reference = storage().ref(`${fileName}.jpeg`);
+    await reference.putFile(`${path}/${fileName}.jpeg`);
+    const url = await reference.getDownloadURL();
 
     return { url, fileName };
 }
